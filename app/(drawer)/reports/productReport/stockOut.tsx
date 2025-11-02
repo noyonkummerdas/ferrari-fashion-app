3bdf6e8855c672d667e40479234bc28d33300e06
Nk Noyon <noyonkummerdas@gmail.com>
2025-11-02 19:22:05 +0600

    report section admin login add all feturch

--- app/(drawer)/reports/productReport/stockOut.tsx
+++ app/(drawer)/reports/productReport/stockOut.tsx
@@ -93,6 +93,8 @@ const { data: productData, error} =
     <View className=" bg-dark p-2 flex-1">
       {/* Filters */}
       <View className="flex-row justify-between items-center mb-4">
+
+        { currentUser?.type === "admin" && warehouses?.length > 0 &&
           <Dropdown
             data={warehouses.map((wh) => ({ label: wh.name, value: wh._id }))}
             labelField="label"
@@ -105,6 +107,7 @@ const { data: productData, error} =
             selectedTextStyle={{ color: "white" }}
             itemTextStyle={{ color: "black" }}
           />
+        }
         {/* From / To Dates */}
         <View className="flex-row gap-3">
           <TouchableOpacity onPress={() => setShowStartPicker(true)} className="p-2 rounded-xl bg-black-200 flex-col items-center">
