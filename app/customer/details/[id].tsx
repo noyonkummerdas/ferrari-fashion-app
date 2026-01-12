8b1ba46e6ed2707312097605e8612d23b74c22fa
Nk Noyon <noyonkummerdas@gmail.com>
2026-01-12 13:29:19 +0600

    some change for customer

--- app/customer/details/[id].tsx
+++ app/customer/details/[id].tsx
@@ -22,7 +22,7 @@ const CustomerDetails = () => {
     isDate:'month',
     forceRefetch: true,
   });
-  // console.log("customer data", data);
+  console.log("customer data", data);
 
   useEffect(() => {
     refetch();
@@ -149,7 +149,7 @@ const CustomerDetails = () => {
             </Text>
           </View> */}
           <View className="flex bg-black-200 items-center justify-center p-5 w-[360px] mx-auto text-center rounded-lg m-1">
-            <Text className="text-white text-xl">Current Balance</Text>
+            <Text className="text-white text-xl">Due Balance</Text>
             <Text className="text-primary font-bold text-lg text-center">
               {data?.customer?.currentBalance} <Text className="text-white">BDT</Text>
             </Text>
