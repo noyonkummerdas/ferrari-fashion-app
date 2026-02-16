caa349dbb91e6df0f3793f786e65634df33fd142
unknown <nknoyon01936@gmail.com>
2026-02-16 11:05:27 +0600

    live url connection

--- constants/baseUrl.js
+++ constants/baseUrl.js
@@ -7,9 +7,9 @@ const IP = "192.168.0.101"
 // export const WS_URL = "ws://192.168.10.225:5001"
 
 // LIVE
-// export const BASE_URL = "https://ffapi.aamardokan.online/api";
-// export const PHOTO_URL = "https://minio.aamardokan.online";
-// export const WS_URL = "wss://api.aamardokan.online";
+export const BASE_URL = "https://ffapi.aamardokan.online/api";
+export const PHOTO_URL = "https://minio.aamardokan.online";
+export const WS_URL = "wss://api.aamardokan.online";
 
 // LIVE
 // export const BASE_URL = "https://telepathy.auroratec.net/api/"
@@ -17,6 +17,6 @@ const IP = "192.168.0.101"
 // export const WS_URL = "wss://telepathy.auroratec.net"
 
 //LOCAL
-export const BASE_URL = `http://${IP}:5001/api`;
-export const PHOTO_URL = `http://${IP}:5001`;
-export const WS_URL = `ws://${IP}:5001`;
+// export const BASE_URL = `http://${IP}:5001/api`;
+// export const PHOTO_URL = `http://${IP}:5001`;
+// export const WS_URL = `ws://${IP}:5001`;
