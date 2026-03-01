c1ac84c65429ab67413bf64e2f0efdfa7606826a
Noyon <nknoyon01936@gmail.com>
2026-03-01 12:46:56 +0600

    currection metro config

--- constants/baseUrl.js
+++ constants/baseUrl.js
@@ -7,16 +7,16 @@ const IP = "192.168.0.163"
 // export const WS_URL = "ws://192.168.10.225:5001"
 
 // LIVE
-// export const BASE_URL = "https://ffapi.aamardokan.online/api";
-// export const PHOTO_URL = "https://minio.aamardokan.online";
-// export const WS_URL = "wss://api.aamardokan.online";
+export const BASE_URL = "https://ffapi.aamardokan.online/api";
+export const PHOTO_URL = "https://minio.aamardokan.online";
+export const WS_URL = "wss://api.aamardokan.online";
 
 // LIVE (Alternate)
 // export const BASE_URL = "https://telepathy.auroratec.net/api/"
 // export const PHOTO_URL = "https://telepathy.auroratec.net"
 // export const WS_URL = "wss://telepathy.auroratec.net"
 
-//LOCAL
-export const BASE_URL = `http://${IP}:5001/api`;
-export const PHOTO_URL = `http://${IP}:5001`;
-export const WS_URL = `ws://${IP}:5001`;
+// //LOCAL
+// export const BASE_URL = `http://${IP}:5001/api`;
+// export const PHOTO_URL = `http://${IP}:5001`;
+// export const WS_URL = `ws://${IP}:5001`;
